8ac6a7249c90fe248f17d3c4b9a635a01f41c6ac
mac <mac@macs-MacBook-Pro-3.local>
2024-12-17 12:21:04 -0500

    cors allow

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from scapy.all import sendp, Ether, ARP, conf
 
 # Initialize the Flask app
 app = Flask(__name__)
+CORS(app)
 
 # Network interface configuration
 interface = None  # Change to match your actual network interface

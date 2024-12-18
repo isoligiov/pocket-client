0664259b3c8cf1b23c663246e83369d1b6071a91
mac <mac@macs-MacBook-Pro-3.local>
2024-12-17 19:14:08 -0500

    channel id

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from scapy.all import sendp, Ether, ARP, conf
 
+CHANNEL_ID = bytes([149])
+
 # Initialize the Flask app
 app = Flask(__name__)
 CORS(app)
@@ -15,17 +17,11 @@ def list_interfaces():
     print(conf.ifaces)
 
 def send_arp_with_extra_data(custom_data):
-    # Construct Ethernet frame
     ether = Ether(dst="ff:ff:ff:ff:ff:ff")  # Broadcast MAC address
-
-    # Construct ARP packet
     arp = ARP(op=1, hwsrc=ether.src, psrc="0.0.0.0", hwdst="00:00:00:00:00:00", pdst="0.0.0.0")
-
-    # Custom data payload
+    channel_id = CHANNEL_ID
     extra_data = custom_data.encode('utf-8')
-
-    # Final packet combining Ethernet, ARP, and extra_data
-    packet = ether / arp / extra_data
+    packet = ether / arp / channel_id / extra_data
 
     # Send packet out on specified interface
     sendp(packet, iface=interface)
@@ -46,3 +42,5 @@ def handle_broadcast():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5030, debug=True)
+
+send_arp_with_extra_data('123')

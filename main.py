19be9dd9f855eebd15f7e540db19432d39db8b11
mac <mac@macs-MacBook-Pro-3.local>
2024-12-19 02:19:17 -0500

    udpated port

--- main.py
+++ main.py
@@ -40,4 +40,4 @@ def handle_broadcast():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5030, debug=True)
+    app.run(host='0.0.0.0', port=5031, debug=True)
